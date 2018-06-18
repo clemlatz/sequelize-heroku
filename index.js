5e29db9ffc97b5b2dec8a9fe6b78b44eab6c766d
Clement Bourgoin <cb@nokto.net>
2018-06-18 12:56:41 +0200

    Simplify code

--- index.js
+++ index.js
@@ -1,66 +1,47 @@
 module.exports = {
 
-    connect: function (Sequelize) {
+  connect: function (Sequelize) {
 
-        if (typeof Sequelize === 'undefined') {
-            throw new Error('You must pass sequelize as an argument to the sequelize-heroku connect method (see README).');
-        }
-
-        var match, config;
-
-        // Look for ClearDB MySQL Add-on
-        if (process.env.CLEARDB_DATABASE_URL) {
-
-            match = process.env.CLEARDB_DATABASE_URL.match(/mysql:\/\/([^:]+):([^@]+)@([^:]+)\/(.+)\?/);
-
-            config = {
-                user: match[1],
-                pass: match[2],
-                base: match[4],
-                options: {
-                    dialect: 'mysql',
-                    protocol: 'mysql',
-                    host: match[3],
-                    port: 3306,
-                    logging: false,
-                    dialectOptions: {
-                        ssl: true
-                    }
-                }
-            };
-
-        }
-
-        // Else, look for Heroky Postgresql
-        else if (process.env.DATABASE_URL) {
-
-            match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
-
-            config = {
-                user: match[1],
-                pass: match[2],
-                base: match[5],
-                options: {
-                    dialect: 'postgres',
-                    protocol: 'postgres',
-                    host: match[3],
-                    logging: false,
-                    port: match[4],
-                    dialectOptions: {
-                        ssl: true
-                    }
-                }
-            };
-
-        }
-
-        if (config) {
-            sequelize = new Sequelize(config.base, config.user, config.pass, config.options);
-
-            return sequelize;
-        }
-
-        return false;
+    if (typeof Sequelize === 'undefined') {
+      throw new Error('You must pass sequelize as an argument to the sequelize-heroku connect method (see README).');
     }
+
+    var url, options;
+
+    // Look for ClearDB MySQL Add-on
+    if (process.env.CLEARDB_DATABASE_URL) {
+
+      url = process.env.CLEARDB_DATABASE_URL;
+
+      options = {
+        dialect: 'mysql',
+        protocol: 'mysql',
+        dialectOptions: {
+          ssl: true
+        }
+      }
+    }
+
+    // Else, look for Heroku Postgres
+    else if (process.env.DATABASE_URL) {
+
+      url = process.env.DATABASE_URL;
+
+      options = {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+          ssl: true
+        }
+      };
+    }
+
+    if (url) {
+      sequelize = new Sequelize(url, options);
+      return sequelize;
+    }
+
+    return false;
+  }
 
 };
