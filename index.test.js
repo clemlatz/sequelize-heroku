dab50a93688aaeaebf00fff64fc0dc59015e163d
Clement Bourgoin <c@iwzr.fr>
2019-04-12 08:18:12 +0200

    Revamp tests using async/await

--- index.test.js
+++ index.test.js
@@ -1,25 +1,17 @@
-if (typeof process.env.DATABASE_URL === 'undefined'
-  && typeof process.env.CLEARDB_DATABASE_URL === 'undefined') {
-  throw new Error('DATABASE_URL or CLEARDB_DATABASE_URL must be defined.')
+if (
+  typeof process.env.DATABASE_URL === "undefined" &&
+  typeof process.env.CLEARDB_DATABASE_URL === "undefined"
+) {
+  throw new Error("DATABASE_URL or CLEARDB_DATABASE_URL must be defined.");
 }
 
-const sequelizeHeroku = require('./index');
-const sequelize = sequelizeHeroku.connect(require('sequelize'));
+const sequelizeHeroku = require("./index");
+const sequelize = sequelizeHeroku.connect(require("sequelize"));
 
-it('connects to database from environnement variables', done => {
-  sequelize
-    .authenticate()
-    .then(function () {
-      sequelize.query('SELECT 1+1 as result').then(function (res) {
-        sequelize.close();
-        const result = res[0][0].result;
-        expect(result).toBe(2);
-        done();
-      });
-    }).catch(function (err) {
-      sequelize.close();
-      var config = sequelize.connectionManager.config;
-      console.log('Sequelize: Error connecting ' + config.host + ' as ' + config.user + ': ' + err);
-      done();
-    });
+it("connects to database from environnement variables", async () => {
+  await sequelize.authenticate();
+  const res = await sequelize.query("SELECT 1+1 as result");
+  sequelize.close();
+  const result = res[0][0].result;
+  expect(result).toBe(2);
 });
