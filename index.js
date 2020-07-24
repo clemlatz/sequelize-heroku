9338219247078331d527dfea0d3baa2806e5c0be
Clément Bourgoin <c@iwzr.fr>
2020-07-24 12:23:31 +0200

    Throw deprecation notice when connect method is called

--- index.js
+++ index.js
@@ -1,42 +1,5 @@
 module.exports = {
-  connect: function(Sequelize) {
-    if (typeof Sequelize === 'undefined') {
-      throw new Error(
-        'You must pass sequelize as an argument to the sequelize-heroku connect method (see README).'
-      );
-    }
-
-    var url, options;
-
-    // Look for ClearDB MySQL Add-on
-    if (process.env.CLEARDB_DATABASE_URL) {
-      url = process.env.CLEARDB_DATABASE_URL;
-
-      options = {
-        dialect: 'mysql',
-        protocol: 'mysql',
-      };
-    }
-
-    // Else, look for Heroku Postgres
-    else if (process.env.DATABASE_URL) {
-      url = process.env.DATABASE_URL;
-
-      options = {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        dialectOptions: {
-          ssl: true,
-        },
-      };
-    }
-
-    if (url) {
-      options.logging = false;
-      sequelize = new Sequelize(url, options);
-      return sequelize;
-    }
-
-    return false;
+  connect: function() {
+    throw new Error('sequelize-heroku is deprecated (see README)')
   },
 };
